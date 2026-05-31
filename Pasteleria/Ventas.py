a55056fe90d3f6b0101f4f054b3bc1cc4d89384a
Geovanx6 <azambrano0417@utm.edu.ed>
2026-05-31 11:29:57 -0500

    Ventas completo

--- Pasteleria/Ventas.py
+++ Pasteleria/Ventas.py
@@ -42,3 +42,31 @@ def buscar_producto():
     
     lista = [{'id': p[0], 'nombre': p[1], 'precio': float(p[2]), 'stock': p[3]} for p in productos]
     return jsonify(lista)
+
+@ventas_blueprint.route('/ventas/procesar_venta', methods=['POST']) # <-- Agregar /ventas
+def procesar_venta():
+    data = request.get_json()
+    id_cliente = data.get('idcliente')
+    items = data.get('items', [])
+    
+    try:
+        cur = mysql.connection.cursor()
+        
+        cur.execute('INSERT INTO factura (fechaFactura, fkcliente) VALUES (CURDATE(), %s)', (id_cliente,))
+        id_factura = cur.lastrowid
+        
+        # Insertar Detalles y Descontar Stock
+        for item in items:
+            cur.execute('''
+                INSERT INTO detalle (fkfactura, fkproducto, cantidad, precioventa) 
+                VALUES (%s, %s, %s, %s)
+            ''', (id_factura, item['id'], item['cantidad'], item['precioVenta']))
+            
+            cur.execute('UPDATE producto SET stock = stock - %s WHERE idproducto = %s', (item['cantidad'], item['id']))
+            
+        mysql.connection.commit()
+        cur.close()
+        return jsonify({'status': 'success', 'message': '¡Venta Guardada con Éxito!', 'nueva_factura': id_factura})
+    except Exception as e:
+        mysql.connection.rollback()
+        return jsonify({'status': 'error', 'message': str(e)}), 500
